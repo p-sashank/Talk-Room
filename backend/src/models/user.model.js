73aef1f52bd8503d5e4c0de47700ad0e57555f22
p-sashank <sashank.perabathula@gmail.com>
2025-08-17 20:13:30 +0530

    Ready

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const { Schema } = mongoose;
+
+const userScheme = new Schema(
+    {
+        name : {type: String, required: true},
+        username : {type: String, required: true, unique: true},
+        password : {type: String, required: true},
+        token : {type: String}
+    }
+)
+
+const User = mongoose.model("User", userScheme);
+
+export {User};
